35af81c473c7d8eaf02e3911e0c7a8bdf5133056
Mohamed elhorafi <m.elhorafi@copag.ma>
2023-03-09 20:15:17 +0100

    create my store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import {AppComponent} from './app.component';
 import {RouterOutlet} from "@angular/router";
 import {HomeComponent} from './home/home.component';
 import {AppRoutingModule} from "./app-routing.module";
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NavbarComponent,
+    ProductsComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    RouterOutlet
+    RouterOutlet,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
